326208489b193c858f4c0c457f3025789e5d60bb
master-dhanesh <dhanesh1296@gmail.com>
2023-04-06 09:28:06 +0530

    Two Way Binding

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,53 @@
 import { useState } from "react";
+import { nanoid } from "nanoid";
 
 const index = () => {
-    // javascript logic
-    let [uname, setuname] = useState("John Doe");
-    const [mydate, setMydate] = useState(new Date().toLocaleTimeString());
+    const [title, setTitle] = useState("");
+    const [desc, setDesc] = useState("");
 
-    setInterval(() => {
-        setMydate(new Date().toLocaleTimeString());
-    }, 1000);
-
-    // let uname = "John Doe";
-
-    const changehandler = () => {
-        console.log("inside change handler");
-        // uname = "Json Doe";
-        setuname("JSON");
+    const CreateTaskHandler = (e) => {
+        e.preventDefault();
+        const todo = {
+            id: nanoid(),
+            title,
+            desc,
+        };
+        console.log(todo);
     };
 
-    // console.log(uname);
+    // const ChangeHandler = (e) => setTitle(e.target.value);
+
     return (
-        <div>
-            <h1>{uname}</h1>
-            <h2>{mydate}</h2>
-            <p>Lorem</p>
-            <button onClick={changehandler}>Change Name</button>
+        <div className="container mt-5 p-5 bg-light">
+            <h2 className="mb-5 fs-2 fw-light">Todo-App</h2>
+
+            <form onSubmit={CreateTaskHandler}>
+                <input
+                    type="text"
+                    placeholder="title"
+                    className="mb-3 w-50 form-control"
+                    onChange={(e) => setTitle(e.target.value)}
+                    value={title}
+                />
+                <input
+                    type="text"
+                    placeholder="Desctiption"
+                    className="mb-3 w-50 form-control"
+                    onChange={(e) => setDesc(e.target.value)}
+                    value={desc}
+                />
+                <button className="btn btn-primary">Create Task</button>
+            </form>
+
+            <hr />
+
+            <ul className="list-group w-50">
+                <h3 className="mb-3 fs-3 fw-light">Pending tasks</h3>
+
+                <li className="fs-4 d-flex justify-content-between list-group-item">
+                    Item 1<i className="text-danger ri-delete-bin-2-line"></i>
+                </li>
+            </ul>
         </div>
     );
 };
