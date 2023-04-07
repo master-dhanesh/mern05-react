09e8a67f04c649d605d3cc48713424c1206e98b0
master-dhanesh <dhanesh1296@gmail.com>
2023-04-07 09:44:51 +0530

    Breaking Component

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,8 @@
+import Create from "@/components/Create";
 import { useState } from "react";
-import { nanoid } from "nanoid";
 
 const index = () => {
-    const [title, setTitle] = useState("");
-    const [desc, setDesc] = useState("");
-
     const [tasks, setTasks] = useState([]);
-
-    const CreateTaskHandler = (e) => {
-        e.preventDefault();
-        const todo = {
-            id: nanoid(),
-            title,
-            desc,
-        };
-        // console.log(todo);
-        // const copyTasks = [...tasks];
-        // copyTasks.push(todo);
-        // setTasks(copyTasks);
-        setTasks([...tasks, todo]);
-        setTitle("");
-        setDesc("");
-    };
 
     const DeleteHandler = (id) => {
         const copyTasks = tasks.filter((task) => task.id !== id);
@@ -51,28 +32,13 @@ const index = () => {
 
     return (
         <div className="container mt-5 p-5 bg-light">
-            <h2 className="mb-5 fs-2 fw-light">Todo-App</h2>
-
-            <form onSubmit={CreateTaskHandler}>
-                <input
-                    type="text"
-                    placeholder="title"
-                    className="mb-3 w-50 form-control"
-                    onChange={(e) => setTitle(e.target.value)}
-                    value={title}
-                />
-                <input
-                    type="text"
-                    placeholder="Desctiption"
-                    className="mb-3 w-50 form-control"
-                    onChange={(e) => setDesc(e.target.value)}
-                    value={desc}
-                />
-                <button className="btn btn-primary">Create Task</button>
-            </form>
+            {/* {Create(tasks,setTasks)} */}
+            {/* <Create tasks={tasks} setTasks={setTasks} /> */}
+            <Create tasks={tasks} setTasks={setTasks}>
+                <h1>Bahut he jyaza secret information</h1>
+            </Create>
 
             <hr />
-
             <ul className="list-group w-50">
                 <h3 className="mb-3 fs-3 fw-light">Pending tasks</h3>
                 {taskslist}
