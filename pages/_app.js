e4477cb825db220569863cc8e29ee81dcbac463b
master-dhanesh <dhanesh1296@gmail.com>
2023-04-17 09:29:45 +0530

    ANT design and TMDB

--- pages/_app.js
+++ pages/_app.js
@@ -2,16 +2,14 @@ import "remixicon/fonts/remixicon.css";
 import "@/node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "@/styles/globals.css";
 
-import Context from "@/Context/Context";
-
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 export default function App({ Component, pageProps }) {
     return (
-        <Context>
+        <>
             <Component {...pageProps} />
             <ToastContainer />
-        </Context>
+        </>
     );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,33 @@
-import Link from "next/link";
+import React, { useEffect } from "react";
+import { Typography, Divider } from "antd";
+import { UserOutlined } from "@ant-design/icons";
+import { Input } from "antd";
+import axios from "axios";
+
+const { Title, Paragraph } = Typography;
 
 const index = () => {
+    const GetLatestMovies = async () => {
+        const { data } = await axios.get(
+            `https://api.themoviedb.org/3/movie/popular?api_key=223667d1239871fc4b6eeef8d0d6def8&language=en-US&page=1`
+        );
+
+        console.log(data);
+    };
+
+    useEffect(() => {
+        GetLatestMovies();
+    }, []);
+
     return (
         <div className="container mt-5 p-5 bg-light">
-            <Link className="fs-1 fw-light mb-3" href="/create-user">
-                Create User
-            </Link>
-            <br />
-            <Link className="fs-1 fw-light mb-3" href="/show-users">
-                Show User
-            </Link>
+            <Title level={4}>Lorem ipsum dolor sit amet.</Title>
+            <Paragraph type="success">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Impedit, at?
+            </Paragraph>
+            <Divider />
+            <Input placeholder="default size" prefix={<UserOutlined />} />
         </div>
     );
 };
