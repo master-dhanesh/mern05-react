c474bcbb6d48d6baf6419e89e00c09b2dbd9067a
master-dhanesh <dhanesh1296@gmail.com>
2023-04-21 20:23:37 +0530

    Framer Motion

--- pages/api/home.js
+++
@@ -1,7 +0,0 @@
-export default function homepage(req, res) {
-    if (req.method === "GET") {
-        res.status(200).json({ message: "Hello from Api GET" });
-    } else if (req.method === "POST") {
-        res.status(200).json({ message: "Hello from Api POST" });
-    }
-}
--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,13 @@
 import React from "react";
-import axios from "axios";
-import Link from "next/link";
-
-const index = (props) => {
+import { motion } from "framer-motion";
+const index = () => {
     return (
         <div className="container mt-5 p-5 bg-light">
-            <ul className="list-group">
-                {props.data &&
-                    props.data.map((i) => (
-                        <li key={i.id} className="list-group-item">
-                            <Link href={`/image-detail/${i.id}`}>
-                                <h1>{i.name}</h1>
-                            </Link>
-                        </li>
-                    ))}
-            </ul>
+            <motion.div whileHover={{ scale: 1.2, x: 100 }}>
+                Hello World
+            </motion.div>
         </div>
     );
 };
-
-export async function getStaticProps(context) {
-    const { data } = await axios.get(
-        `https://jsonplaceholder.typicode.com/users`
-    );
-    return {
-        props: { data }, // will be passed to the page component as props
-    };
-}
 
 export default index;
